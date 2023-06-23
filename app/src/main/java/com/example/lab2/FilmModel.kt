a7df67274c50ada99c8b00c09f7c009727269906
DESKTOP-EFAPJ4T\ThekonorBlade <vprotsun@gmail.com>
2023-06-23 12:35:14 +0300

    1

--- app/src/main/java/com/example/lab2/FilmModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lab2
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class FilmModel(val name: String, val year: Int, val image: Int, val about: String): Parcelable
